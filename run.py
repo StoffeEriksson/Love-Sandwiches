a54f7589c62b16dca35dc6764552d4194076abdd
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-05-15 13:22:27 +0200

    make get_stock_values

--- run.py
+++ run.py
@@ -138,6 +138,11 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").row_values(1)
+
+    return {heading: value for heading, value in zip(headings, data)}
+
 
 def main():
     """
@@ -151,6 +156,10 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print("Make the following numbers of sandwiches for next market:\n")
+    print(stock_values)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
